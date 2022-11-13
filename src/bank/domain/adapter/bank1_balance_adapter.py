186575dea5808913defd86b3e8249f96d433274b
Eduardo Shibukawa <eduardoshibuka@gmail.com>
2022-11-13 20:34:00 +0000

    refactor(bank): :recycle: making adapter fields final

--- src/bank/domain/adapter/bank1_balance_adapter.py
+++ src/bank/domain/adapter/bank1_balance_adapter.py
@@ -1,7 +1,10 @@
+from typing import Final
+
+
 class Bank1BalanceAdapter:
     def __init__(self, acccount_balance: float, currency: str):
-        self.__account_balance__ = acccount_balance
-        self.__currency__ = currency
+        self.__account_balance__: Final = acccount_balance
+        self.__currency__: Final = currency
 
     def balance(self) -> float:
         return self.__account_balance__
--- src/bank/domain/adapter/bank1_transaction_adapter.py
+++ src/bank/domain/adapter/bank1_transaction_adapter.py
@@ -1,19 +1,23 @@
+from typing import Final
 from src.integration.bank1.bank1_transaction import Bank1Transaction
 from src.bank.domain.transaction_type import TransactionType
 
 
 class Bank1TransactionAdapter:
+
     def __init__(self, bank1_transaction: Bank1Transaction):
-        self.__transaction__ = bank1_transaction
+        self.__amount__: Final = bank1_transaction.amount
+        if bank1_transaction.type == 1:
+            self.__type__: Final = TransactionType.CREDIT
+        elif bank1_transaction.type == 2:
+            self.__type__: Final = TransactionType.DEBIT
+        self.__text__: Final = bank1_transaction.text
 
     def amount(self) -> float:
-        return self.__transaction__.amount
+        return self.__amount__
 
     def type(self) -> TransactionType:
-        if self.__transaction__.type == 1:
-            return TransactionType.CREDIT
-        elif self.__transaction__.type == 2:
-            return TransactionType.DEBIT
+        return self.__type__
 
     def text(self) -> str:
-        return self.__transaction__.text
+        return self.__text__
