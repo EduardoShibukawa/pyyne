26cb9cf44bf280c57ba9d8734204459416580985
Eduardo Shibukawa <eduardoshibuka@gmail.com>
2022-11-13 19:09:03 +0000

    feat(bank): :package: add bank domain files

---
+++ src/bank/domain/BankAccountBalance.py
@@ -0,0 +1,4 @@
+class BankAccountBalance:
+    def __init__(self, balance, currency):
+        self.balance = balance
+        self.currency = currency
---
+++ src/bank/domain/BankAccountTransaction.py
@@ -0,0 +1,6 @@
+class BankAccountTransaction:
+
+    def __init__(self, amount, type, text):
+        self.amount = amount
+        self.type = type
+        self.text = text
