eecf0af147ff4bdf897693c980bae4c540fdb40f
Eduardo Shibukawa <eduardoshibuka@gmail.com>
2022-11-13 18:26:45 +0000

    feat(bank): :tada: creating bank controller
    creating bank controller with stub information

---
+++ src/bank/bank_controller.py
@@ -0,0 +1,6 @@
+class BankController:
+    def print_balances(self):
+        return "Implement me to pull balance information from all available bank integrations and display them, one after the other."
+
+    def print_transcations(self):
+        print("Implement me to pull transactions from all available bank integrations and display them, one after the other.")
