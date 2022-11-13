6f55b528f354376811381714b2ac892213a31f99
Eduardo Shibukawa <eduardoshibuka@gmail.com>
2022-11-13 20:25:38 +0000

    refactor(bank): :recycle: refactor bank 1 transaction adapter to use private variable

--- src/bank/domain/adapter/bank1_transaction_adapter.py
+++ src/bank/domain/adapter/bank1_transaction_adapter.py
@@ -4,16 +4,16 @@ from src.bank.domain.transaction_type import TransactionType
 
 class Bank1TransactionAdapter:
     def __init__(self, bank1_transaction: Bank1Transaction):
-        self.transaction = bank1_transaction
+        self.__transaction__ = bank1_transaction
 
     def amount(self) -> float:
-        return self.transaction.amount
+        return self.__transaction__.amount
 
     def type(self) -> TransactionType:
-        if self.transaction.type == 1:
+        if self.__transaction__.type == 1:
             return TransactionType.CREDIT
-        elif self.transaction.type == 2:
+        elif self.__transaction__.type == 2:
             return TransactionType.DEBIT
 
-    def text(self):
-        return self.transaction.text
+    def text(self) -> str:
+        return self.__transaction__.text
